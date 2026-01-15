026eaee98e077e6385c569d2aa1daac4d4233fee
Mohammed Ali <codebyali17@gmail.com>
2026-01-15 13:31:26 +0530

    test responsivenss

--- component/TestimonialsSection.js
+++ component/TestimonialsSection.js
@@ -85,7 +85,7 @@ const TestimonialsSection = () => {
           </div>
           <motion.div
             initial={{ opacity: 0, y: 30 }}
-            animate={isInView ? { opacity: 1, y: 0 } : {}}
+            // animate={isInView ? { opacity: 1, y: 0 } : {}}
             transition={{ duration: 0.8 }}
           >
             <h2 className="text-6xl md:text-8xl font-medium text-primary mb-2 lg:mb-6">
